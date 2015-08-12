70c1c0f2673ca9765760146c0d979c70444ed612
ryosuke <tmcit.miyazaki@gmail.com>
2015-08-12 17:47:09 +0900

    [add] add theme of zsh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
-
-    ln -sv "$f" "$HOME"/"$f"
+    cd
+    ln -sv "dotfiles/$f" "$f"
+    cd -
 done
+
+cd
+ln -sv dotfiles/wedisagree_ryosuke.zsh-theme ./.oh-my-zsh/themes/wedisagree_ryosuke.zsh-theme
+
