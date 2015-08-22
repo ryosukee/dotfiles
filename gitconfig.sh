6789487aed92c1f36eb78b9f0c324ee38038f53b
ryosukee <tmcit.miyazaki@gmail.com>
2015-08-22 21:16:06 +0900

    [add] git config file

---
+++ gitconfig.sh
@@ -0,0 +1,6 @@
+git config --global user.name ryosukee
+git config --global user.email tmcit.miyazaki@gmail.com
+git config --global alias.bra "branch -vva"
+git config --global color.ui true
+git config --global push.default simple
+git config --global core.editer vim
