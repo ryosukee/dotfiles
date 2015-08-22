c0b47b578124ad6019b0dea4b32b88719f403804
ryosukee <tmcit.miyazaki@gmail.com>
2015-08-22 21:38:31 +0900

    [add] monor

--- gitconfig.sh
+++ gitconfig.sh
@@ -3,4 +3,5 @@ git config --global user.email tmcit.miyazaki@gmail.com
 git config --global alias.bra "branch -vva"
 git config --global color.ui true
 git config --global push.default simple
-git config --global core.editer vim
+git config --global core.editor 'vim -c "set fenc=utf-8"'
+
