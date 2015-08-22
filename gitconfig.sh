6f4a6c63c6d4bb530b1a9df6c123be83ee5a8ca1
ryosukee <tmcit.miyazaki@gmail.com>
2015-08-23 00:32:38 +0900

    [add] git log alias

--- gitconfig.sh
+++ gitconfig.sh
@@ -4,4 +4,5 @@ git config --global alias.bra "branch -vva"
 git config --global color.ui true
 git config --global push.default simple
 git config --global core.editor 'vim -c "set fenc=utf-8"'
+git config --global alias.l 'log --stat'
 
