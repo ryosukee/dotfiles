54df588a01c7e73ec7a6739e7351744d3a56c518
ryosukee <tmcit.miyazaki@gmail.com>
2015-11-03 03:21:36 +0900

    [fix] name

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 echo "change login shell to /bin/zsh"
-chsh /bin/zsh
+chsh -s /bin/zsh
 
 echo "setup oh-my-zsh"
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 echo "setup dotfiles"
 git clone https://github.com/ryosukee/dotfiles ~/.dotfiles
 
-ln -sfv ~/dotfiles/wedisagree_ryosuke.zsh-theme ~/.oh-my-zsh/themes/wedisagree_ryosuke.zsh-theme
+ln -sfv ~/.dotfiles/wedisagree_ryosuke.zsh-theme ~/.oh-my-zsh/themes/wedisagree_ryosuke.zsh-theme
 
-for f in `ls -a ~/dotfiles | grep "^\.[^./]"`
+for f in `ls -a ~/.dotfiles | grep "^\.[^./]"`
 do
     [ "$f" = ".git" ] && continue
-    ln -sfv "~/dotfiles/$f" "~/$f"
+    ln -sfv "~/.dotfiles/$f" "~/$f"
 done
 
