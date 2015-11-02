d25386eea9a5b3b3f854b7345e37acfaf9530773
ryosukee <tmcit.miyazaki@gmail.com>
2015-11-03 02:52:47 +0900

    [add] several update

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-for f in .??*
+echo "change login shell to /bin/zsh"
+chsh /bin/zsh
+
+echo "setup oh-my-zsh"
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+echo "setup dotfiles"
+git clone https://github.com/ryosukee/dotfiles ~/.dotfiles
+
+ln -sfv ~/dotfiles/wedisagree_ryosuke.zsh-theme ~/.oh-my-zsh/themes/wedisagree_ryosuke.zsh-theme
+
+for f in `ls -a | grep "^\.[^./]"`
 do
     [ "$f" = ".git" ] && continue
-    cd
-    ln -sv "dotfiles/$f" "$f"
-    cd -
+    ln -sfv "~/dotfiles/$f" "~/$f"
 done
-
-cd
-cp dotfiles/wedisagree_ryosuke.zsh-theme ./.oh-my-zsh/themes/wedisagree_ryosuke.zsh-theme
 
