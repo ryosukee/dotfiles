e921605315a693197ee760ccd05ef5fc5a9d2924
ryosukee <tmcit.miyazaki@gmail.com>
2015-11-03 03:02:08 +0900

    [fix] ls dir name

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ git clone https://github.com/ryosukee/dotfiles ~/.dotfiles
 
 ln -sfv ~/dotfiles/wedisagree_ryosuke.zsh-theme ~/.oh-my-zsh/themes/wedisagree_ryosuke.zsh-theme
 
-for f in `ls -a | grep "^\.[^./]"`
+for f in `ls -a ~/dotfiles | grep "^\.[^./]"`
 do
     [ "$f" = ".git" ] && continue
     ln -sfv "~/dotfiles/$f" "~/$f"
