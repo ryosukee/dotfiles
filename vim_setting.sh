480b8f5738c1b69deab819b6667692ee8229d3c6
ryosukee <tmcit.miyazaki@gmail.com>
2015-11-07 20:11:34 +0900

    [add] download plugins

--- vim_setting.sh
+++ vim_setting.sh
@@ -1,3 +1,6 @@
 echo 'download neobundle'
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+echo 'download plugins'
+vim -u ~/.vimrc -i NONE -c "try | NeoBundleUpdate! | finally | q! | endtry" -e -s -V1
