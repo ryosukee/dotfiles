581ea7a789a270f2439eb31bb454a0798a6aff6b
ryosukee <tmcit.miyazaki@gmail.com>
2015-11-07 19:43:52 +0900

    [fix] sh->bash

--- install.sh
+++ install.sh
@@ -4,25 +4,25 @@ git clone https://github.com/ryosukee/dotfiles ~/.dotfiles
 
 echo
 echo "================ Zsh setting ================"
-sh ~/.dotfiles/zsh_setting.sh
+bash ~/.dotfiles/zsh_setting.sh
 echo "============================================="
 
 
 echo
 echo "============= Dotfiles setting ============="
-sh ~/.dotfiles/dotfiles_setting.sh
+bash ~/.dotfiles/dotfiles_setting.sh
 echo "============================================="
 
 
 echo
 echo "=============== pyenv setting ==============="
-sh ~/.dotfiles/pyenv_setting.sh
+bash ~/.dotfiles/pyenv_setting.sh
 echo "============================================="
 
 
 echo
 echo "=============== vim setting ================="
-sh ~/.dotfiles/vim_setting.sh
+bash ~/.dotfiles/vim_setting.sh
 echo "============================================="
 
 
