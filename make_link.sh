33b0c2a273a7bfc29c0224a6313690bc9e640b27
ryosukee <tmcit.miyazaki@gmail.com>
2015-11-07 18:37:56 +0900

    [add] add make link shell

---
+++ make_link.sh
@@ -0,0 +1,7 @@
+
+ln -sfv ~/.dotfiles/wedisagree_ryosuke.zsh-theme ~/.oh-my-zsh/themes/wedisagree_ryosuke.zsh-theme
+for f in `ls -a ~/.dotfiles| grep "^\.[^./]"`
+do
+    [ "$f" = ".git" ] && continue
+    ln -sfv ~/.dotfiles/$f ~/$f
+done
